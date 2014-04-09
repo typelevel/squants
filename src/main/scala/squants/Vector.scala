d573b3b421d7bdaed5026a6a248a0f84d4d2479f
garyKeorkunian <gsk@gara.com>
2014-04-09 17:22:24 -0400

    First functioning implementation of Vector

--- src/main/scala/squants/Vector.scala
+++ src/main/scala/squants/Vector.scala
@@ -8,42 +8,204 @@
 
 package squants
 
+import squants.motion.{ MetersPerSecondSquared, NewtonSeconds }
+
 /**
- * Experimental implementation of 3-Dimensional Vector Quantity
+ * Root trait for representing Vectors
+ *
+ * EXPERIMENTAL
  *
  * @author garyKeorkunian
  * @since 0.3.0
  *
- * @param x A
- * @param y A
- * @param z A
- * @tparam A Quantity Type
+ * @tparam A Type for the vector's coordinate values
  */
-case class Vector[A <: Quantity[A]](x: A, y: A, z: A) {
+trait Vector[A] {
+  /**
+   * The list values that makeup Vector's coordinates
+   * @return
+   */
+  def coordinates: Seq[A]
 
-  def length()(implicit create: Double ⇒ A): A =
-    create(math.sqrt(x.value * x.value + y.value * y.value + z.value * z.value))
+  /**
+   * The scalar value of the Vector
+   * @return
+   */
+  def magnitude: A
 
-  def plus(that: Vector[A]): Vector[A] = Vector(x + that.x, y + that.y, z + that.z)
-  def +(that: Vector[A]) = plus(that)
+  /**
+   * Creates the Unit Vector which corresponds to this vector
+   * @return
+   */
+  def normalize: Vector[A]
 
-  def minus(that: Vector[A]): Vector[A] = Vector(x - that.x, y - that.y, z - that.z)
-  def -(that: Vector[A]) = minus(that)
+  /**
+   * Add two Vectors
+   * @param that Vector[A]
+   * @return
+   */
+  def plus(that: Vector[A]): Vector[A]
+  def + = plus _
 
-  def times(that: Double): Vector[A] = Vector(x * that, y * that, z * that)
-  def *(that: Double) = times(that)
+  /**
+   * Subtract two Vectors
+   * @param that Vector[A]
+   * @return
+   */
+  def minus(that: Vector[A]): Vector[A]
+  def - = minus _
 
-  def divide(that: Double): Vector[A] = Vector(x / that, y / that, z / that)
+  /**
+   * Scale a Vector
+   * @param that Double
+   * @return
+   */
+  def times(that: Double): Vector[A]
+  def * = times _
+
+  /**
+   * Reduce a Vector
+   *
+   * @param that Double
+   * @return
+   */
+  def divide(that: Double): Vector[A]
   def /(that: Double) = divide(that)
 
-  def dotProduct(that: Vector[A])(implicit create: Double ⇒ A): A =
-    create(x.value * that.x.value + y.value * that.y.value + z.value * that.z.value)
+  /**
+   * Reduce and "de-unit" a Vector.  Returns a DoubleVector
+   * @param that Quantity[A]
+   * @return
+   */
+  def divide(that: A): Vector[Double]
+  def /(that: A) = divide(that)
 
-  def crossProduct(that: Vector[A])(implicit create: Double ⇒ A): Vector[A] =
-    Vector(
-      create(y.value * that.z.value - z.value * that.y.value),
-      create(z.value * that.x.value - x.value * that.z.value),
-      create(x.value * that.y.value - y.value * that.x.value))
+  /**
+   * Create the Dot Product of two Vectors
+   * @param that Vector[A]
+   * @return
+   */
+  def dotProduct(that: Vector[A]): A
+  def *(that: Vector[A]) = dotProduct(that)
 
-  def normalize(implicit create: Double ⇒ A): Vector[A] = this divide length().value
+  /**
+   * Create the Cross Product of two Vectors
+   * @param that Vector[A]
+   * @return
+   */
+  def crossProduct(that: Vector[A]): Vector[A]
 }
+
+/**
+ * Double (Real Number) Vector
+ *
+ * EXPERIMENTAL
+ *
+ * @author garyKeorkunian
+ * @since 0.3.0
+ *
+ * @param coordinates Double *
+ */
+case class DoubleVector(coordinates: Double*) extends Vector[Double] {
+  def magnitude: Double = math.sqrt(coordinates.toTraversable.map(v ⇒ v * v).sum)
+  def normalize: Vector[Double] = this.divide(magnitude)
+  def plus(that: Vector[Double]): Vector[Double] =
+    DoubleVector(coordinates.toIterable.zipAll(that.coordinates.toIterable, 0d, 0d).toTraversable.map(v ⇒ v._1 + v._2).toSeq: _*)
+  def minus(that: Vector[Double]): Vector[Double] =
+    DoubleVector(coordinates.toIterable.zipAll(that.coordinates.toIterable, 0d, 0d).toTraversable.map(v ⇒ v._1 - v._2).toSeq: _*)
+  def times(that: Double): Vector[Double] = DoubleVector(coordinates.toTraversable.map(v ⇒ v * that).toSeq: _*)
+  def divide(that: Double): Vector[Double] = DoubleVector(coordinates.toTraversable.map(v ⇒ v / that).toSeq: _*)
+
+  def dotProduct(that: Vector[Double]) =
+    coordinates.toIterable.zipAll(that.coordinates.toIterable, 0d, 0d).toTraversable.map(v ⇒ v._1 * v._2).sum
+
+  def crossProduct(that: Vector[Double]) = (this.coordinates.length, that.coordinates.length) match {
+    case (3, 3) ⇒
+      DoubleVector(coordinates(1) * that.coordinates(2) - coordinates(2) * that.coordinates(1),
+        coordinates(2) * that.coordinates(0) - coordinates(0) * that.coordinates(2),
+        coordinates(0) * that.coordinates(1) - coordinates(1) * that.coordinates(0))
+    case (7, 7) ⇒ throw new UnsupportedOperationException("Seven-dimensional cross product is not currently supported")
+    case _      ⇒ throw new UnsupportedOperationException("Cross product is not supported on vectors with an arbitrary number of dimensions")
+  }
+}
+
+/**
+ * Quantity Vector
+ *
+ * EXPERIMENTAL
+ *
+ * @author garyKeorkunian
+ * @since 0.3.0
+ *
+ * @param coordinates Variable list of A
+ * @tparam A QuantityType
+ */
+case class QuantityVector[A <: Quantity[A]](unit: UnitOfMeasure[A], coordinates: A*) extends Vector[A] {
+  def magnitude: A = unit(math.sqrt(coordinates.toTraversable.map(v ⇒ v.to(unit) * v.to(unit)).sum))
+  def normalize: Vector[A] = this / magnitude.to(unit)
+  def plus(that: Vector[A]): Vector[A] =
+    QuantityVector(unit, coordinates.toIterable.zipAll(that.coordinates.toIterable, unit(0), unit(0)).toTraversable.map(v ⇒ v._1 + v._2).toSeq: _*)
+  def minus(that: Vector[A]): Vector[A] =
+    QuantityVector(unit, coordinates.toIterable.zipAll(that.coordinates.toIterable, unit(0), unit(0)).toTraversable.map(v ⇒ v._1 - v._2).toSeq: _*)
+  def times(that: Double): Vector[A] =
+    QuantityVector(unit, coordinates.toTraversable.map(v ⇒ v * that).toSeq: _*)
+  def divide(that: Double): Vector[A] =
+    QuantityVector(unit, coordinates.toTraversable.map(v ⇒ v / that).toSeq: _*)
+  def divide(that: A): Vector[Double] =
+    DoubleVector(coordinates.toTraversable.map(v ⇒ v / that).toSeq: _*)
+
+  def dotProduct(that: Vector[A]): A =
+    unit(coordinates.toIterable.zipAll(that.coordinates.toIterable, unit(0), unit(0)).toTraversable.map(v ⇒ v._1.to(unit) * v._2.to(unit)).sum)
+
+  def crossProduct(that: Vector[A]): Vector[A] = (this.coordinates.length, that.coordinates.length) match {
+    case (3, 3) ⇒
+      QuantityVector(unit,
+        unit(coordinates(1).to(unit) * that.coordinates(2).to(unit) -
+          coordinates(2).to(unit) * that.coordinates(1).to(unit)),
+        unit(coordinates(2).to(unit) * that.coordinates(0).to(unit) -
+          coordinates(0).to(unit) * that.coordinates(2).to(unit)),
+        unit(coordinates(0).to(unit) * that.coordinates(1).to(unit) -
+          coordinates(1).to(unit) * that.coordinates(0).to(unit)))
+    case (7, 7) ⇒ throw new UnsupportedOperationException("Seven-dimensional cross product is not currently supported")
+    case _      ⇒ throw new UnsupportedOperationException("Cross product is not supported on vectors with an arbitrary number of dimensions")
+  }
+}
+
+/**
+ * Strategy #1 for implementations that support dimensional conversions
+ *
+ * Add implicit conversions for each Quantity type
+ *
+ */
+object QuantityVectorConversion {
+
+  implicit class VelocityVector(v: QuantityVector[Velocity]) {
+    def *(that: Mass): QuantityVector[Momentum] =
+      QuantityVector(NewtonSeconds, v.coordinates.toTraversable.map(v ⇒ v * that).toSeq: _*)
+
+    def /(that: Time): QuantityVector[Acceleration] =
+      QuantityVector(MetersPerSecondSquared, v.coordinates.toTraversable.map(v ⇒ v / that).toSeq: _*)
+
+    def /(that: Acceleration): QuantityVector[Time] =
+      QuantityVector(Seconds, v.coordinates.toTraversable.map(v ⇒ v / that).toSeq: _*)
+  }
+}
+
+/**
+ * Strategy #2 for implementations that support dimensional conversions
+ *
+ * Implement a class representing the specific Quantity Vector
+ *
+ */
+//case class VelocityVector(override val unit: UnitOfMeasure[Velocity], velocities: Velocity*)
+//    extends QuantityVector[Velocity](unit, velocities: _*) {
+//
+//  def *(that: Mass): QuantityVector[Momentum] =
+//    QuantityVector(NewtonSeconds, velocities.toTraversable.map(v ⇒ v * that).toSeq: _*)
+//
+//  def /(that: Time): QuantityVector[Acceleration] =
+//    QuantityVector(MetersPerSecondSquared, velocities.toTraversable.map(v ⇒ v / that).toSeq: _*)
+//
+//  def /(that: Acceleration): QuantityVector[Time] =
+//    QuantityVector(Seconds, velocities.toTraversable.map(v ⇒ v / that).toSeq: _*)
+//}
--- src/test/scala/squants/VectorSpec.scala
+++ src/test/scala/squants/VectorSpec.scala
@@ -23,32 +23,31 @@ class VectorSpec extends FlatSpec with Matchers {
   behavior of "Vector"
 
   it should "create a Vector with expected values" in {
-    val vector = Vector(Kilometers(1), Kilometers(10), Kilometers(5))
-    vector.x should be(Kilometers(1))
-    vector.y should be(Kilometers(10))
-    vector.z should be(Kilometers(5))
+    val vector = QuantityVector(Kilometers, Kilometers(1), Kilometers(10), Kilometers(5))
+    vector.coordinates(0) should be(Kilometers(1))
+    vector.coordinates(1) should be(Kilometers(10))
+    vector.coordinates(2) should be(Kilometers(5))
   }
 
   it should "equate to like Vectors" in {
     val x = Kilometers(1)
     val y = Kilometers(2)
     val z = Kilometers(3)
-    Vector(x, y, z).equals(Vector(x, y, z)) should be(right = true)
-    Vector(x, y, z) == Vector(x, y, z) should be(right = true)
+    QuantityVector(Kilometers, x, y, z).equals(QuantityVector(Kilometers, x, y, z)) should be(right = true)
+    QuantityVector(Kilometers, x, y, z) == QuantityVector(Kilometers, x, y, z) should be(right = true)
   }
 
   it should "not equate to dislike Vectors" in {
     val x = Kilometers(1)
     val y = Kilometers(2)
     val z = Kilometers(3)
-    Vector(x, y, z).equals(Vector(z, y, z)) should be(right = false)
-    Vector(x, y, z) != Vector(z, y, z) should be(right = true)
+    QuantityVector(Kilometers, x, y, z).equals(QuantityVector(Kilometers, z, y, z)) should be(right = false)
+    QuantityVector(Kilometers, x, y, z) != QuantityVector(Kilometers, z, y, z) should be(right = true)
   }
 
   it should "determine a length" in {
-    implicit val create: Double ⇒ Length = value ⇒ Meters(value)
-    val vector = Vector(Meters(3), Meters(4), Meters(5))
-    vector.length should be(Meters(math.sqrt(3 * 3 + 4 * 4 + 5 * 5)))
+    val vector = QuantityVector(Meters, Meters(3), Meters(4), Meters(5))
+    vector.magnitude should be(Meters(math.sqrt(3 * 3 + 4 * 4 + 5 * 5)))
   }
 
   it should "add two Vectors" in {
@@ -56,7 +55,7 @@ class VectorSpec extends FlatSpec with Matchers {
     val y = Kilometers(2)
     val z = Kilometers(3)
     val a = Kilometers(5)
-    Vector(x, y, z).plus(Vector(a, a, a)) should be(Vector(x + a, y + a, z + a))
+    QuantityVector(Kilometers, x, y, z).plus(QuantityVector(Kilometers, a, a, a)) should be(QuantityVector(Kilometers, x + a, y + a, z + a))
   }
 
   it should "subtract two Vectors" in {
@@ -64,7 +63,7 @@ class VectorSpec extends FlatSpec with Matchers {
     val y = Kilometers(2)
     val z = Kilometers(3)
     val a = Kilometers(5)
-    Vector(x, y, z).minus(Vector(a, a, a)) should be(Vector(x - a, y - a, z - a))
+    QuantityVector(Kilometers, x, y, z).minus(QuantityVector(Kilometers, a, a, a)) should be(QuantityVector(Kilometers, x - a, y - a, z - a))
   }
 
   it should "rescale a Vector" in {
@@ -72,38 +71,42 @@ class VectorSpec extends FlatSpec with Matchers {
     val y = Kilometers(2)
     val z = Kilometers(3)
     val r = 5
-    Vector(x, y, z).times(r) should be(Vector(x * r, y * r, z * r))
-    Vector(x, y, z) * r should be(Vector(x * r, y * r, z * r))
-    Vector(x, y, z).divide(r) should be(Vector(x / r, y / r, z / r))
-    Vector(x, y, z) / r should be(Vector(x / r, y / r, z / r))
+    QuantityVector(Kilometers, x, y, z).times(r) should be(QuantityVector(Kilometers, x * r, y * r, z * r))
+    QuantityVector(Kilometers, x, y, z) * r should be(QuantityVector(Kilometers, x * r, y * r, z * r))
+    QuantityVector(Kilometers, x, y, z).divide(r) should be(QuantityVector(Kilometers, x / r, y / r, z / r))
+    QuantityVector(Kilometers, x, y, z) / r should be(QuantityVector(Kilometers, x / r, y / r, z / r))
   }
 
   it should "dot product two Vectors" in {
-    implicit val create: Double ⇒ Length = value ⇒ Meters(value)
     val x = Kilometers(1)
     val y = Kilometers(2)
     val z = Kilometers(3)
     val a = Kilometers(5)
-    Vector(x, y, z).dotProduct(Vector(a, a, a)) should be(Meters(x.value * a.value + y.value * a.value + z.value * a.value))
+    val expRes = Kilometers(x.to(Kilometers) * a.to(Kilometers) + y.to(Kilometers) * a.to(Kilometers) + z.to(Kilometers) * a.to(Kilometers))
+    QuantityVector(Kilometers, x, y, z).dotProduct(QuantityVector(Kilometers, a, a, a)) should be(expRes)
+    QuantityVector(Kilometers, x, y, z) * QuantityVector(Kilometers, a, a, a) should be(expRes)
   }
 
-  it should "cross product two Vectors" in {
-    implicit val create: Double ⇒ Length = value ⇒ Meters(value)
+  it should "cross product two Vectors with 3 coordinates each" in {
     val x = Kilometers(1)
     val y = Kilometers(2)
     val z = Kilometers(3)
     val a = Kilometers(5)
-    Vector(x, y, z).crossProduct(Vector(a, a, a)) should be(
-      Vector(Meters(y.value * a.value - z.value * a.value),
-        Meters(z.value * a.value - x.value * a.value),
-        Meters(x.value * a.value - y.value * a.value)))
+    val expRes = QuantityVector(Kilometers,
+      Kilometers(y.to(Kilometers) * a.to(Kilometers) - z.to(Kilometers) * a.to(Kilometers)),
+      Kilometers(z.to(Kilometers) * a.to(Kilometers) - x.to(Kilometers) * a.to(Kilometers)),
+      Kilometers(x.to(Kilometers) * a.to(Kilometers) - y.to(Kilometers) * a.to(Kilometers)))
+    QuantityVector(Kilometers, x, y, z).crossProduct(QuantityVector(Kilometers, a, a, a)) should be(expRes)
   }
+
+  it should "cross product two Vectors with 7 coordinates each" is pending
 
   it should "normalize a Vector" in {
-    implicit val create: Double ⇒ Length = value ⇒ Meters(value)
-    val x = Kilometers(1)
-    val y = Kilometers(2)
+    val x = Kilometers(3)
+    val y = Kilometers(3)
     val z = Kilometers(3)
-    Vector(x, y, z).normalize.length() should be(Meters(1.0))
+    val normalized = QuantityVector(Kilometers, x, y, z).normalize
+    normalized.magnitude should be(Kilometers(1.0))
+    println(normalized)
   }
 }
