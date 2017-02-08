08735c1273fb7f152c750ade50806b8fd9dafad5
Derek Morr <derekmorr@psu.edu>
2017-02-08 09:54:20 -0500

    don't parse multiple degree symbols and be more flexible in whitespace when parsing temperature

--- shared/src/main/scala/squants/thermal/Temperature.scala
+++ shared/src/main/scala/squants/thermal/Temperature.scala
@@ -141,7 +141,7 @@ object Temperature extends Dimension[Temperature] with BaseDimension {
   def apply[A](n: A, scale: TemperatureScale)(implicit num: Numeric[A]) = new Temperature(num.toDouble(n), scale)
 
   def apply(s: String): Try[Temperature] = {
-    val regex = "([-+]?[0-9]*\\.?[0-9]+(?:[eE][-+]?[0-9]+)?)[ °]*(f|F|c|C|k|K|r|R)".r
+    val regex = "([-+]?[0-9]*\\.?[0-9]+(?:[eE][-+]?[0-9]+)?)*[ ]*°? *(f|F|c|C|k|K|r|R)".r
     s match {
       case regex(value, Fahrenheit.symbol) ⇒ Success(Fahrenheit(value.toDouble))
       case regex(value, "f")               ⇒ Success(Fahrenheit(value.toDouble))
