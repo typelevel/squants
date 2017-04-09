ac70c2f6446a95018eac6947142ca47baededc67
Derek Morr <derek.morr.test@gmail.com>
2017-04-09 14:22:50 -0400

    add basic tests for DefaultFormatter

---
+++ shared/src/test/scala/squants/formatter/DefaultFormatterSpec.scala
@@ -0,0 +1,22 @@
+package squants.formatter
+
+import org.scalatest.{FlatSpec, Matchers}
+import squants.space.{Centimeters, Length, Meters, Kilometers}
+import squants.space.LengthConversions._
+import squants.units.SiUnitGroups.SiLengths
+
+class DefaultFormatterSpec extends FlatSpec with Matchers {
+
+  behavior of "DefaultFormatter"
+
+  it should "pick the best unit" in {
+    val siLengthFormatter = new DefaultFormatter[Length] { val units = SiLengths }
+
+    // keeping the unit the same
+    siLengthFormatter.bestUnit(5.cm).unit should be (Centimeters)
+
+    // changing the unit
+    siLengthFormatter.bestUnit(500.cm).unit should be (Meters)
+    siLengthFormatter.bestUnit(3000.meters).unit should be (Kilometers)
+  }
+}
