46fe801f5032fe738456eeacf6b222a16be5ee73
Derek Morr <derek.morr.test@gmail.com>
2017-04-27 07:56:22 -0400

    reformat lines

--- shared/src/main/scala/squants/unitgroups/information/package.scala
+++ shared/src/main/scala/squants/unitgroups/information/package.scala
@@ -11,15 +11,13 @@ package object information {
   /** Base-2 units for Information defined by the IEC */
   object IECInformation extends UnitGroup[Information] {
     val units: Set[UnitOfMeasure[Information]] =
-      Set(Bytes, Kibibytes, Mebibytes, Gibibytes,
-        Tebibytes, Pebibytes, Exbibytes, Zebibytes, Yobibytes)
+      Set(Bytes, Kibibytes, Mebibytes, Gibibytes, Tebibytes, Pebibytes, Exbibytes, Zebibytes, Yobibytes)
   }
 
   /** Base-10 units for Information */
   object MetricInformation extends UnitGroup[Information] {
     val units: Set[UnitOfMeasure[Information]] =
-      Set(Bytes, Kilobytes, Megabytes, Gigabytes,
-        Terabytes, Petabytes, Exabytes, Zettabytes, Yottabytes)
+      Set(Bytes, Kilobytes, Megabytes, Gigabytes, Terabytes, Petabytes, Exabytes, Zettabytes, Yottabytes)
   }
 
 }
