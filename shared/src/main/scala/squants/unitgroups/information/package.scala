389de5223c0be00e06867b41de0ed81dac6ff6d3
Derek Morr <derek.morr.test@gmail.com>
2017-05-12 00:49:29 -0400

    SI unit groups; misc other unit groups

---
+++ shared/src/main/scala/squants/unitgroups/information/package.scala
@@ -0,0 +1,23 @@
+package squants.unitgroups
+
+import squants.UnitOfMeasure
+import squants.information._
+
+/**
+  * UnitGroups for [[Information]]
+  */
+package object information {
+
+  /** Base-2 units for Information defined by the IEC */
+  object IECInformation extends UnitGroup[Information] {
+    val units: Set[UnitOfMeasure[Information]] =
+      Set(Bytes, Kibibytes, Mebibytes, Gibibytes, Tebibytes, Pebibytes, Exbibytes, Zebibytes, Yobibytes)
+  }
+
+  /** Base-10 units for Information */
+  object MetricInformation extends UnitGroup[Information] {
+    val units: Set[UnitOfMeasure[Information]] =
+      Set(Bytes, Kilobytes, Megabytes, Gigabytes, Terabytes, Petabytes, Exabytes, Zettabytes, Yottabytes)
+  }
+
+}
