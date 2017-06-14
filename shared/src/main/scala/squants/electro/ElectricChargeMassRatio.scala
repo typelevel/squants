a150df334e114463bb9f373e9ad16c541ee122cd
underscorenico <vinuesa.nico@gmail.com>
2017-06-14 21:19:03 +0200

    add AreaElectricChargeDensity, ElectricChargeDensity, ElectricChargeMassRatio and LinearElectricChargeDensity types

---
+++ shared/src/main/scala/squants/electro/ElectricChargeMassRatio.scala
@@ -0,0 +1,48 @@
+package squants.electro
+
+import squants.mass.Kilograms
+import squants.{AbstractQuantityNumeric, Dimension, Mass, PrimaryUnit, Quantity, SiUnit, UnitConverter, UnitOfMeasure}
+
+/**
+  *
+  * @author Nicolas Vinuesa
+  * @since 1.4
+  *
+  * @param value Double
+  */
+final class ElectricChargeMassRatio private (val value: Double, val unit: ElectricChargeMassRatioUnit)
+  extends Quantity[ElectricChargeMassRatio] {
+
+  def dimension = ElectricChargeMassRatio
+
+  def *(that: Mass): ElectricCharge = Coulombs(this.toCoulombsKilograms * that.toKilograms)
+
+  def toCoulombsKilograms = to(CoulombsPerKilogram)
+}
+
+object ElectricChargeMassRatio extends Dimension[ElectricChargeMassRatio] {
+  private[electro] def apply[A](n: A, unit: ElectricChargeMassRatioUnit)(implicit num: Numeric[A]) = new ElectricChargeMassRatio(num.toDouble(n), unit)
+  def apply = parse _
+  def name = "ElectricChargeMassRatio"
+  def primaryUnit = CoulombsPerKilogram
+  def siUnit = CoulombsPerKilogram
+  def units = Set(CoulombsPerKilogram)
+}
+
+trait ElectricChargeMassRatioUnit extends UnitOfMeasure[ElectricChargeMassRatio] with UnitConverter {
+  def apply[A](n: A)(implicit num: Numeric[A]) = ElectricChargeMassRatio(n, this)
+}
+
+object CoulombsPerKilogram extends ElectricChargeMassRatioUnit with PrimaryUnit with SiUnit {
+  val symbol = Coulombs.symbol + "/" + Kilograms.symbol
+}
+
+object ElectricChargeMassRatioConversions {
+  lazy val coulombPerKilogram = CoulombsPerKilogram(1)
+
+  implicit class ElectricChargeMassRatioConversions[A](n: A)(implicit num: Numeric[A]) {
+    def coulombsPerKilogram = CoulombsPerKilogram(n)
+  }
+
+  implicit object ElectricChargeMassRatioNumeric extends AbstractQuantityNumeric[ElectricChargeMassRatio](ElectricChargeMassRatio.primaryUnit)
+}
---
+++ shared/src/test/scala/squants/electro/AreaElectricChargeDensitySpec.scala
@@ -0,0 +1,65 @@
+package squants.electro
+
+import org.scalatest.{FlatSpec, Matchers}
+import squants.QuantityParseException
+import squants.space.{Meters, SquareMeters}
+
+/**
+  *
+  * @author Nicolas Vinuesa
+  * @since 1.4
+  *
+  */
+class AreaElectricChargeDensitySpec extends FlatSpec with Matchers {
+
+  behavior of "AreaElectricChargeDensity and its Units of Measure"
+
+  it should "create values using UOM factories" in {
+    CoulombsPerSquareMeter(1).toCoulombsSquareMeters should be(1)
+  }
+
+  it should "create values from properly formatted Strings" in {
+    AreaElectricChargeDensity("10.22 C/m²").get should be(CoulombsPerSquareMeter(10.22))
+    AreaElectricChargeDensity("10.22 zz").failed.get should be(QuantityParseException("Unable to parse AreaElectricChargeDensity", "10.22 zz"))
+    AreaElectricChargeDensity("zz C/m²").failed.get should be(QuantityParseException("Unable to parse AreaElectricChargeDensity", "zz C/m²"))
+  }
+
+  it should "properly convert to all supported Units of Measure" in {
+    val x = CoulombsPerSquareMeter(10)
+    x.toCoulombsSquareMeters should be(10.0)
+  }
+
+  it should "return properly formatted strings for all supported Units of Measure" in {
+    CoulombsPerSquareMeter(1).toString(CoulombsPerSquareMeter) should be("1.0 C/m²")
+  }
+
+  it should "return ElectricCharge when multiplied by Area" in {
+    CoulombsPerSquareMeter(1) * SquareMeters(1) should be(Coulombs(1))
+  }
+
+  it should "return LinearElectricChargeDensity when multiplied by Length" in {
+    CoulombsPerSquareMeter(1) * Meters(1) should be(CoulombsPerMeter(1))
+  }
+
+  behavior of "AreaElectricChargeDensityConversions"
+
+  it should "provide aliases for single unit values" in {
+    import AreaElectricChargeDensityConversions._
+
+    coulombPerSquareMeter should be(CoulombsPerSquareMeter(1))
+  }
+
+  it should "provide implicit conversion from Double" in {
+    import AreaElectricChargeDensityConversions._
+
+    val d = 10.22
+    d.coulombsPerSquareMeter should be(CoulombsPerSquareMeter(d))
+  }
+
+  it should "provide Numeric support" in {
+    import AreaElectricChargeDensityConversions.AreaElectricChargeDensityNumeric
+
+    val rs = List(CoulombsPerSquareMeter(100), CoulombsPerSquareMeter(10))
+    rs.sum should be(CoulombsPerSquareMeter(110))
+  }
+}
