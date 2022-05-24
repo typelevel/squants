f3c4759b476850c52eec988add09e2c035998000
garyKeorkunian <GaryKeorkunian@gmail.com>
2022-05-23 21:43:21 -0700

    Create Squants1 to Squants2 Dimension code generator.

--- shared/src/main/scala/squants2converter/Squants2Converter.scala
+++ shared/src/main/scala/squants2converter/Squants2Converter.scala
@@ -12,8 +12,8 @@ import squants.space._
 import squants.thermal._
 import squants.time._
 
-
-import java.io.{ FileWriter, PrintWriter }
+import java.io.{ File, FileWriter, PrintWriter }
+import java.nio.file.{ Files, Path }
 
 object Squants2Converter extends App {
 
@@ -48,9 +48,13 @@ object Squants2Converter extends App {
     Dimensionless
   )
 
-  val file = new FileWriter("UNITS.md")
-  printAllDimensions(new PrintWriter(file))
-  file.close()
+  writeDimensionFile(Frequency)
+
+  def writeUnitsDoc(): Unit = {
+    val file = new FileWriter("UNITS.md")
+    printAllDimensions(new PrintWriter(file))
+    file.close()
+  }
 
   def printAllDimensions(printer: PrintWriter): Unit = {
 
@@ -99,6 +103,65 @@ object Squants2Converter extends App {
     }
 
     printer.flush()
+  }
+
+  def writeDimensionFile(d: Dimension[_]): Unit = {
+
+    val packageName = d.getClass.getPackage.getName.replace("squants.", "")
+    val path = s"shared/src/main/scala/squants2converter/$packageName/"
+    if(!Files.exists(Path.of(path))) Files.createDirectory(Path.of(path))
+    val file = new File(s"$path${d.name}.scala")
+    val writer = new PrintWriter(file)
+
+    writer.println(s"package ${d.getClass.getPackage.getName.replace("squants", "squants2")}")
+    writer.println()
+    writer.println("import squants2._")
+    writer.println()
+
+    writer.println(s"final case class ${d.name}[A: Numeric] private [squants2]  (value: A, unit: ${d.name}Unit)")
+    writer.println(s"  extends Quantity[A, ${d.name}.type] {")
+    writer.println(s"  override type Q[B] = ${d.name}[B]")
+    writer.println(s"}")
+    writer.println()
+
+    writer.println(s"object ${d.name} extends Dimension(\"${d.name}\") {")
+    writer.println()
+    writer.println(s"  override def primaryUnit: UnitOfMeasure[this.type] with PrimaryUnit = ${d.primaryUnit.getClass.getSimpleName.replace("$", "")}")
+    writer.println(s"  override def siUnit: UnitOfMeasure[this.type] with SiUnit = ${d.siUnit.getClass.getSimpleName.replace("$", "")}")
+    val unitList = d.units.toList
+      .sortBy{(u: UnitOfMeasure[_]) => u.convertFrom(1d)}
+      .map { (u: UnitOfMeasure[_]) => s"${u.getClass.getSimpleName.replace("$", "")}"}
+      .mkString(", ")
+    writer.println(s"  override lazy val units: Set[UnitOfMeasure[this.type]] = ")
+    writer.println(s"    Set(${unitList})")
+    writer.println()
+    writer.println(s"  implicit class ${d.name}Cons[A](a: A)(implicit num: Numeric[A]) {")
+    d.units.toList
+      .sortBy{(u: UnitOfMeasure[_]) => u.convertFrom(1d)}
+      .foreach { (u: UnitOfMeasure[_]) =>
+        val unitName = u.getClass.getSimpleName.replace("$", "")
+        writer.println(s"    def ${unitName.head.toLower + unitName.tail}: ${d.name}[A] = $unitName(a)")
+      }
+    writer.println(s"  }")
+
+    writer.println(s"}")
+    writer.println()
+    writer.println(s"abstract class ${d.name}Unit(val symbol: String, val conversionFactor: ConversionFactor) extends UnitOfMeasure[${d.name}.type] {")
+    writer.println(s"  override lazy val dimension: ${d.name}.type = ${d.name}")
+    writer.println(s"  override def apply[A: Numeric](value: A): ${d.name}[A] = ${d.name}(value, this)")
+    writer.println(s"}")
+    writer.println()
+    d.units.toList
+      .sortBy{(u: UnitOfMeasure[_]) => u.convertFrom(1d)}
+      .foreach { (u: UnitOfMeasure[_]) =>
+        writer.print(s"case object ${u.getClass.getSimpleName.replace("$", "")} extends ${d.name}Unit(\"${u.symbol}\", ${u.convertFrom(1d)})")
+        if(u.isInstanceOf[PrimaryUnit]) writer.print(" with PrimaryUnit")
+        if(u.isInstanceOf[SiUnit]) writer.print(" with SiUnit")
+        writer.println()
+    }
+
+    writer.flush()
+    writer.close()
   }
 
 
